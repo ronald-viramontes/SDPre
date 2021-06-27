29222d22cade6a925e672429e4d551bfd95f4ddc
ronald-viramontes <ronald.viramontes@gmail.com>
2021-06-26 21:44:53 -0400

    files added

---
+++ Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 5;
+    num2 = 7;
+    num3 = 4;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
