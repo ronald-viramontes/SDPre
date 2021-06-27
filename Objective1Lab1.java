8ac27b16882f0a5c16e43fa218fae715fd2737fb
ronald-viramontes <ronald.viramontes@gmail.com>
2021-06-26 21:44:02 -0400

    new files added

---
+++ Objective1Lab1.java
@@ -0,0 +1,8 @@
+public class Objective1Lab1 {
+  public static void main(String[] args) {
+      System.out.println("Hello, Skill Distillery");
+    /*
+      Your solution goes here
+    */
+  }
+}
---
+++ Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ronald R. Viramontes";
+    int age = 41;
+    String state = "North Carolina";
+    System.out.print("Name: "); System.out.print(name); System.out.print("  Age: "); System.out.print(age); System.out.print("  State: "); System.out.print(state);
+  }
+}
