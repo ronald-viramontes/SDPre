a33948007518bae43bedfd4789e946f8a17bebb6
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-07 14:49:59 -0400

    MenuExample.java

---
+++ MenuExample.java
@@ -0,0 +1,9 @@
+public class MenuExample {
+  public static void main(String[] args) {
+    System.out.println("Welcome to My Example Menu");
+    System.out.println("--------------------------");
+    System.out.println("1) Check the Weather");
+    System.out.println("2) Check the News");
+    System.out.println("3) Check the Stock Market");
+  }
+}
