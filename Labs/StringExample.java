20e2b7e2e090ff89542257be21d6482aaf332108
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-07 14:46:39 -0400

    Labs/StringExample.java

---
+++ Labs/StringExample.java
@@ -0,0 +1,8 @@
+public class StringExample {
+  public static void main(String[] args) {
+    String withoutQuotes = "This is a sentence without quotes.";
+    String withQuotes = "This is a sentence with \"quotes\".";
+    System.out.println(withoutQuotes);
+    System.out.println(withQuotes);
+  }
+}
