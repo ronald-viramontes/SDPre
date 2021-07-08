27966f9df63bca35e1bfed6e5de2dee076959b45
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-08 19:11:18 -0400

    Labs/Objective7Lab1.java

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,9 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 100;
+    while(counter > 0) {
+      System.out.println("The current number is " + counter +"!");
+      counter = counter - 1;
+    }
+  }
+}
