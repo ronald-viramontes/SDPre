d5fd73e917ed4120133a28037315944fa44139e6
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-08 18:59:53 -0400

    WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+  public static void main(String[] args) {
+    int num = 0;
+
+    while (num < 50) {
+      System.out.println("The number is " + num);
+      num = num + 3;
+    }
+  }
+}
