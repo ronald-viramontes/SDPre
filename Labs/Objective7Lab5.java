fbd890181da28fdeaf372be4369387e44414193f
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-09 01:27:04 -0400

    Labs/Objective7Lab5.java

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,28 @@
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    int sel;
+
+    do {
+      System.out.println("____Menu____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My Favorite Foods");
+      System.out.println("3: Exit");
+      System.out.println();
+      sel = scanner.nextInt();
+
+      if (sel == 1) {
+        System.out.println("1");
+        System.out.println("Hello World");
+          break; }
+      if (sel == 2) {
+        System.out.println("2");
+        System.out.println("Apple, Banana, Coconut");
+          break; }
+      if (sel == 3) { 
+        System.out.println("3");
+        System.out.println("Goodbye");
+         break; }
+    } while (sel >= 1 && sel < 3);
+  }
+}
