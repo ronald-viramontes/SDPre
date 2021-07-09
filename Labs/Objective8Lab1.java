573df539c2bb915b116510c7abe3abf8293a6074
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-09 12:53:51 -0400

    Labs/Objective8Lab1.java

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+    for (i = 10; i >= 1; i--) {
+      System.out.println(i);
+    }
+  }
+}
